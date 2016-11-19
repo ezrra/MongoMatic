9233db6845033f651229d28e74663bafb382de11
Israel Martinez <israel.martinez.vargas@gmail.com>
2016-11-18 18:06:16 -0800

    App updated

--- app/README.md
+++ app/README.md
@@ -9,3 +9,8 @@ $ npm install
 ```
 $ node --max-old-space-size=20000 server.js
 ```
+
+## Using nodemon
+```
+$ nodemon .\server.js localhost 3000 --max-old-space-size=20000
+```
--- app/public/js/app.js
+++ app/public/js/app.js
@@ -1,55 +1,137 @@
 (function () {
 
-	var app = angular.module('App', []);
+	var app = angular.module('App', ['daterangepicker']);
 
 	app.controller('MainCtrl', function ($scope, $http) {
 
-		$scope.loader = true;
+		$scope.loader = false;
 
-		var dates = {
-			startDate: '2016-01-01',
-			endDate: '2016-01-31'
+		$scope.daterangepicker = true;
+
+		$scope.currentData = [];
+
+		$scope.data = [];
+
+		function requests (startDate, endDate, callback) {
+
+			var dates = { startDate: startDate, endDate: endDate };
+
+			var req = { method: 'GET', url: '/api/test', params: dates };
+
+			$http(req).then(function (response) {
+
+				callback(null, response)
+
+			}, function (err) {
+
+				callback(err, [])
+
+			});
 		};
 
-		var req = {
-			method: 'GET',
-		 	url: '/api/test',
-		 	params: dates
-		}
+		var dates = [
+			{ text: "Ayer", endDate: moment(), startDate: moment().subtract(1, 'days') },
+			{ text: "Hace 7 dias", endDate: moment(), startDate: moment().subtract(6, 'days') },
+			{ text: "Hace 14 dias", endDate: moment(), startDate: moment().subtract(13, 'days') },
+			{ text: "Hace 30 dias", endDate: moment(), startDate: moment().subtract(29, 'days') }
+		];
 
-		$http(req).then(function (response) {
-
-			$scope.data = response.data;
-
-			console.log(response)
-
-			if ($scope.data.length == 0) {
-				
-				alert("No hay datos para mostrar")
+		angular.forEach(dates, function (value, key) {
 			
-			} else {
+			var startDate 	= moment(value.startDate).format("2015-MM-DD"),
+				endDate 	= moment(value.endDate).format("2015-MM-DD");
 
-				pivotUI();
-			}
+			requests(startDate, endDate, function (err, response) {
 
-			$scope.loader = false;
+				if (key == 0) {  $scope.currentData = response.data; pivotUI(); };
 
-		}, function (err) {
+				$scope.opts.ranges[value.text] = [startDate, endDate];
 
-			console.log(err);
+				$scope.data.push({
+					'text': value.text,
+					'data': response.data,
+					'startDate': startDate,
+					'endDate': endDate
+				})
+
+			})
 
 		});
 
-		function pivotUI () {
+		$scope.$watch('data', function (newData, oldData) {
+			
+			console.log(newData)
 
+		}, true);
+
+		function pivotUI () {
 			$("#output").pivotUI(
-			    $scope.data,
+			    $scope.currentData,
 			    {
 			        rows: ["DatabaseSource"],
 			        cols: [""],
 			    }, false, "es"
 			);
 		}
+
+		$scope.date = {
+	        startDate: moment().subtract(1, "days"),
+	        endDate: moment()
+	    };
+
+	    $scope.opts = {
+	        locale: {
+	            applyClass: 'btn-green',
+	            applyLabel: "Aplicar",
+	            fromLabel: "De",
+	            format: "YYYY-MM-DD",
+	            toLabel: "a",
+	            cancelLabel: 'Cancelar',
+	            customRangeLabel: 'Rango',
+	            "monthNames": [
+		            "January",
+		            "February",
+		            "March",
+		            "April",
+		            "May",
+		            "June",
+		            "July",
+		            "August",
+		            "September",
+		            "October",
+		            "November",
+		            "December"
+		        ],
+		        "daysOfWeek": [
+		            "Dom",
+		            "Lun",
+		            "Mar",
+		            "Mic",
+		            "Jue",
+		            "Vie",
+		            "Sab"
+		        ],
+	        },
+	        ranges: {},
+	    };
+
+	    $scope.$watch('date', function(date) {
+
+	        var startDate 	= moment(date.startDate).format("YYYY-MM-DD");
+	        var endDate 	= moment(date.endDate).format("YYYY-MM-DD");
+	        var index 		= _.findKey($scope.data, { 'startDate': startDate, 'endDate': endDate });
+
+	        $scope.currentData = [];
+
+	        if ($scope.data.length > 0) {
+
+	        	$scope.currentData = $scope.data[index].data;
+	        }
+
+	        // pivotUI();
+
+	    }, false);
+
 
 	});
 
